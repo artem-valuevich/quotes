64c832886fa8d19aef168e89ea0b8b9411ce94f6
artem-valuevich <artem.valuevich@mail.ru>
2021-03-21 21:53:27 +0300

    first version of slider navigator

--- js/Slider.js
+++ js/Slider.js
@@ -4,8 +4,22 @@ class Slider {
     this.currentSlide = 0;
     this.images = this.container.querySelectorAll("img");
     this.imagesAmount = this.images.length;
+
     this.resize();
     setInterval(this.switch.bind(this), 3000);
+
+    this.circles = document.createElement("div");
+    this.circles.classList.add("circles");
+
+    for (let i = 0; i < this.imagesAmount; i++) {
+      const circle = document.createElement("div");
+      circle.classList.add("circle");
+      if (!i) circle.classList.add("active");
+      circle.addEventListener("click", this.switch.bind(this, i));
+      this.circles.insertAdjacentElement("beforeend", circle);
+    }
+
+    node.insertAdjacentElement("afterend", this.circles);
   }
 
   resize() {
@@ -15,11 +29,19 @@ class Slider {
     });
   }
 
-  switch() {
-    this.currentSlide++;
+  switch(toSlide) {
+    const prev = this.currentSlide++;
     if (this.currentSlide >= this.imagesAmount) {
       this.currentSlide = 0;
     }
+    if (toSlide !== undefined) {
+      this.currentSlide = toSlide;
+    }
+    this.circles
+      .querySelectorAll(".circle")
+      [this.currentSlide].classList.toggle("active");
+
+    this.circles.querySelectorAll(".circle")[prev].classList.toggle("active");
     this.container.style = `transform: translateX(-${
       this.width * this.currentSlide
     }px)`;
