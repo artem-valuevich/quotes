319800ca0cb4c8b325ae4bc2faa26042d7362fbe
artem-valuevich <artem.valuevich@mail.ru>
2021-04-20 14:15:37 +0300

    menu added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ window.onresize = () => {
   slider.resize();
   slider1.resize();
 };
+
+const isMobile = /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iP(ad|od|hone)/i.test(
+  navigator.userAgent
+);
+
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector("#menu");
+
+if (isMobile) {
+  menu.classList.add("isMobile");
+  menuButton.addEventListener("click", () => {
+    document.body.classList.toggle("no-overflow");
+    menu.classList.toggle("hidden");
+    menuButton.classList.toggle("black");
+  });
+} else {
+  menu.addEventListener("mouseleave", () => {
+    menu.classList.add("hidden");
+    menuButton.classList.remove("black");
+  });
+
+  menuButton.addEventListener("mouseenter", () => {
+    menu.classList.remove("hidden");
+    menuButton.classList.add("black");
+  });
+}
